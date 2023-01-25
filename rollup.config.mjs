438b443316eddedfa6e5f3cd677cacc1b1c98e1a
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-25 13:02:52 -0500

    Enable source map support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,14 @@
 import json from '@rollup/plugin-json';
 import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
   input: 'index.js',
   output: {
 		file: 'index.dist.js',
-    format: 'cjs'
+    format: 'cjs',
+    sourcemap: true
   },
-  plugins: [json(), commonjs(), nodeResolve({ preferBuiltins: true })]
+  plugins: [json(), commonjs(), nodeResolve({ preferBuiltins: true }), sourcemaps()]
 };
