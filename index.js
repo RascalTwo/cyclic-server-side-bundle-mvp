1c91303904efc356c96d504296e49a98da948180
Rascal_Two <therealrascaltwo@gmail.com>
2023-01-25 12:09:10 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { format } = require('date-fns');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use('*', (request, response) => {
+	return response.send(`Hello ${request.method} request from ${request.originalUrl}, today is a ${format(new Date, 'eeee')}`);
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}/`));
+
